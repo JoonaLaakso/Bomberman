4557513d12c1026c37f45f37e61afd25db194e7f
unknown <joona.laakso@peli03-pc.opisto.hkro>
2016-04-20 10:02:09 +0300

    more comments for functionality

--- LaajasaloBomberman/Assets/BombDetonate.cs
+++ LaajasaloBomberman/Assets/BombDetonate.cs
@@ -37,6 +37,27 @@ public class BombDetonate : MonoBehaviour {
         //if we hit a player, bomb sprite keeps going but we run the death script from the player. 
         //bomb center uses sprite #1, extensions use sprite #2 and where bomb ends we use sprite #3
 
+        /*#1 Init:
+        create the center mass of the bomb, set up a loop. For the loop we need these variables:
+        -initial location
+        -temp variables for every direction (simple as only one vector2 variable), these might have to be rounded
+        -bool to check for each direction (list of 4)
+        -make the loop check 4 different directions (up, down left right with x, -x, y, -y)
+
+        #2 make explosions;
+        explosion objects need a is a trigger collider with a script which checks if ontriggerenter happens (for players, also might need a list for players)
+        loop logic; check if spot is either a block or a free spot. for soft blocks; create end particle on them. For hard blocks, create end particle before them.
+        This can be easily done if we check the spot before making the prefab to the spot.
+
+        if we want to go right for example on the 2nd loop, we do:
+        exploSpot =  vector2(tempx + i, tempy)
+        if left:
+        exploSpot = vector2(tempx - i, tempy)
+        if up:
+        exploSpot = vector2(tempx, tempy + i)
+        */
+
+
     }
 }
 
